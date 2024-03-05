12750d6f0310332e4c245f8978775f385a7f225a
terp <95259598+terpdoctor@users.noreply.github.com>
2024-03-04 21:44:57 -0800

    Update teleburn.rs

--- src/subcommand/teleburn.rs
+++ src/subcommand/teleburn.rs
@@ -1,20 +1,51 @@
-use super::*;
+use {super::*, crate::index::entry::Entry};
+use base58::ToBase58;
 
 #[derive(Debug, Parser)]
 pub(crate) struct Teleburn {
-  #[arg(help = "Generate teleburn addresses for inscription <DESTINATION>.")]
-  destination: InscriptionId,
+@@ -8,11 +9,15 @@ pub(crate) struct Teleburn {
+#[derive(Debug, PartialEq, Serialize)]
+pub struct Output {
+  ethereum: EthereumTeleburnAddress,
+  solana: SolanaTeleburnAddress,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct Output {
-  pub ethereum: crate::teleburn::Ethereum,
+#[derive(Debug, PartialEq)]
+struct EthereumTeleburnAddress([u8; 20]);
+
+#[derive(Debug, PartialEq)]
+struct SolanaTeleburnAddress([u8; 32]);
+
+impl Serialize for EthereumTeleburnAddress {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+@@ -34,11 +39,29 @@ impl Display for EthereumTeleburnAddress {
+  }
+}
+
+impl Serialize for SolanaTeleburnAddress {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: Serializer,
+  {
+    serializer.collect_str(self)
+  }
+}
+
+impl Display for SolanaTeleburnAddress {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    write!(f, "{}", self.0.to_base58())?;
+
+    Ok(())
+  }
 }
 
 impl Teleburn {
-  pub(crate) fn run(self) -> SubcommandResult {
-    Ok(Box::new(Output {
-      ethereum: self.destination.into(),
-    }))
+  pub(crate) fn run(self) -> Result {
+    let digest = bitcoin::hashes::sha256::Hash::hash(&self.recipient.store());
+    print_json(Output {
+      ethereum: EthereumTeleburnAddress(digest[0..20].try_into().unwrap()),
+      solana: SolanaTeleburnAddress(digest[0..32].try_into().unwrap()),
+    })?;
+    Ok(())
   }
-}
